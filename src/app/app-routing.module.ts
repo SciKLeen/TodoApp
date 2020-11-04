ce6cdd6ff9ff8be5cf3859d996a7e0c5bceeb2d1
Minh Hoàng Hồ Sỹ <minhhoang97124>
2020-11-04 14:38:07 +0700

    Add login and todo-list component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'todo-list', component: TodoListComponent },
+  { path: '',   redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: LoginComponent },
+  { path: '**', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,51 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+  MatSelectModule,
+  MatIconModule,
+  MatDialogModule,
+  MatCheckboxModule,
+  MatListModule,
+  MatMenuModule,
+  MatToolbarModule
+} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {InputTextModule} from 'primeng/inputtext';
+
+import { LoginComponent } from './components/login/login.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TodoListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatIconModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatListModule,
+    MatMenuModule,
+    MatToolbarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
