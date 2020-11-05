5973c1a3ea6a4a2295a2170709d2f2a001ddd665
Minh Hoàng Hồ Sỹ <minhhoang97124>
2020-11-05 11:16:45 +0700

    Add todo Modals

---
+++ src/app/modals/todo.ts
@@ -0,0 +1,14 @@
+export interface IList {
+  id: number;
+  name: string;
+  items: number;
+}
+
+// export interface ITask {
+//   id: number;
+//   listId: number;
+//   name: string;
+//   completed: boolean;
+//   createdAt: string;
+//   completedAt: string;
+// }
